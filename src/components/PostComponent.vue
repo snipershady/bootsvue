0191c2226f4fa4422d0b8da94386760268196138
snipershady <snipershady@gmail.com>
2025-06-15 15:15:12 +0100

    ref and computed

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const tableData = ref([]);
+const tableDateude = ref([]); //variabile inutile
+const tableDataLen = computed(()=> tableData.value.length)
 
 function getData() {
   return fetch("http://local.sym7vuebe.com/post/get-all")
@@ -11,12 +13,38 @@ function getData() {
     });
 }
 
+
 getData();
+
+setTimeout(() => {
+  tableData.value.push({
+    id: 1,
+    title: "La compagnia dell'anello",
+    body: "mammt",
+    date: "2024-08-22T10:04:45+00:00",
+    featured: true,
+    subtitle: "di JRR Tolkien",
+    isArchived: false,
+    slug: "la-compagnia-dell-anello",
+    views: 0,
+  });
+}, 2000);
 </script>
 
 <template>
   <div>
     <h2>Elenco Utenti - Tabella</h2>
+
+    <div class="row">
+      <div class="col">
+        <label for="cars">Choose a car:</label>
+        <select class="form-select" name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+        </select>
+      </div>
+    </div>
+
     <div v-if="tableData.length > 0">
       <table class="table table-striped">
         <thead class="table-success">
@@ -36,6 +64,10 @@ getData();
           </tr>
         </tbody>
       </table>
+      <div class="row">
+        <div class="col">Numero di righe computed = {{ tableDataLen }}</div>
+        <div class="col">Numero di righe  = {{ tableData.length }}</div>
+      </div>
     </div>
     <div v-else>Loading...</div>
   </div>
