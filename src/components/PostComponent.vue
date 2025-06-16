5570524017680f94765d051bcf4b994c24afff59
snipershady <snipershady@gmail.com>
2025-06-16 15:17:06 +0100

    Menu select

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -2,20 +2,33 @@
 import { computed, ref } from "vue";
 
 const tableData = ref([]);
-const tableDateude = ref([]); //variabile inutile
-const tableDataLen = computed(()=> tableData.value.length)
+const isArchived = ref(0);
+
+const tableDataLen = computed(() => tableData.value.length);
 
 function getData() {
-  return fetch("http://local.sym7vuebe.com/post/get-all")
+  return fetch(`http://local.sym7vuebe.com/post/get-all?is_archived=${isArchived.value}`)
     .then((response) => response.json())
     .then((json) => {
       tableData.value = json;
     });
 }
 
+function archivePost(id) {
+  fetch(`http://local.sym7vuebe.com/post/${id}/archive`, { method: "POST" })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("Post archiviato:", json);
+      getData(); // Aggiorna la lista dopo l'archiviazione
+    });
+}
+
+
+
 
 getData();
 
+/*
 setTimeout(() => {
   tableData.value.push({
     id: 1,
@@ -29,6 +42,7 @@ setTimeout(() => {
     views: 0,
   });
 }, 2000);
+*/
 </script>
 
 <template>
@@ -37,11 +51,12 @@ setTimeout(() => {
 
     <div class="row">
       <div class="col">
-        <label for="cars">Choose a car:</label>
-        <select class="form-select" name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
+        <label for="cars">Filtra i dati</label>
+        <select class="form-control" v-model="isArchived">
+          <option :value="1">Archiviate</option>
+          <option :value="0">Attive</option>
         </select>
+        <button class="btn btn-success mt-3 mb-5" @click="getData">Carica dati</button>
       </div>
     </div>
 
@@ -53,6 +68,7 @@ setTimeout(() => {
             <th>Titolo</th>
             <th>Sottotitolo</th>
             <th>Testo</th>
+            <th>Azioni</th>
           </tr>
         </thead>
         <tbody>
@@ -61,12 +77,15 @@ setTimeout(() => {
             <td>{{ post.title }}</td>
             <td>{{ post.subtitle }}</td>
             <td>{{ post.body }}</td>
+            <td>
+              <button class="btn btn-warning" @click="archivePost(post.id)">Archive</button>
+            </td>
           </tr>
         </tbody>
       </table>
       <div class="row">
         <div class="col">Numero di righe computed = {{ tableDataLen }}</div>
-        <div class="col">Numero di righe  = {{ tableData.length }}</div>
+        <div class="col">Numero di righe = {{ tableData.length }}</div>
       </div>
     </div>
     <div v-else>Loading...</div>
